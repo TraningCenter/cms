af36589f0a95f8272bab72654a1b7a86f3fbbbff
Alegerd <alexgerasimov.post@gmail.com>
2018-03-11 01:21:27 +0300

    + allPostsPage

---
+++ UI/src/main/java/dto/PostDto.java
@@ -0,0 +1,47 @@
+package dto;
+
+public class PostDto {
+
+    private String title;
+    private String author;
+    private String photo;
+    private String post;
+
+    public PostDto(String title, String author, String post) {
+        this.title = title;
+        this.author = author;
+        this.post = post;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+}
--- UI/src/main/java/views/IndexPageView.java
+++ UI/src/main/java/views/IndexPageView.java
@@ -1,25 +1,31 @@
 package views;
 
+import dto.PostDto;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import java.util.ArrayList;
+import java.util.List;
 
 @ManagedBean
 @SessionScoped
 public class IndexPageView {
 
-    public String helloWorld;
+    public List<PostDto> posts;
 
     @PostConstruct
     public void init(){
-        helloWorld = "Sosat";
+        posts = new ArrayList<>();
+        posts.add(new PostDto("title", "Author's Name", "Lorem Ipsum post content"));
+        posts.add(new PostDto("title2", "Author's Name2", "Lorem Ipsum post content"));
+        posts.add(new PostDto("title3", "Author's Name3", "Lorem Ipsum post content"));
+        posts.add(new PostDto("title4", "Author's Name4", "Lorem Ipsum post content"));
+        posts.add(new PostDto("title5", "Author's Name5", "Lorem Ipsum post content"));
+        posts.add(new PostDto("title6", "Author's Name6", "Lorem Ipsum post content"));
     }
 
-    public String getHelloWorld() {
-        return helloWorld;
-    }
-
-    public void setHelloWorld(String helloWorld) {
-        this.helloWorld = helloWorld;
+    public List<PostDto> getPosts() {
+        return posts;
     }
 }
